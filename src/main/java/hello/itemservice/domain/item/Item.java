01d321a1bbf93f92aa72dec49add79bd9574576d
CJ <choijun0627@konkuk.ac.kr>
2023-08-02 15:32:54 +0900

    <feat> 스프링 MVC 2편 프로젝트 section 2
    단일 체크박스 사용
    
    단일 체크박스 사용하기 위해 addForm.html에 체크박스 추가
    로그 찍어보기 위해 FormItemController에 로그 추가
    http 요청 메시지 보기 위해 application.properties에 설정 추가
    
    html의 단일 체크박스에 불편한 점으로 미체크시 값이 안 넘어오는 것을 히든
    필드를 추가해서 편리하게 하는 것 배움.

--- src/main/java/hello/itemservice/domain/item/Item.java
+++ src/main/java/hello/itemservice/domain/item/Item.java
@@ -16,7 +16,7 @@ public class Item {
 
     public Item() {
     }
-    
+
     public Item(String itemName, Integer price, Integer quantity) {
         this.itemName = itemName;
         this.price = price;
